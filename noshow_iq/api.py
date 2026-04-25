28349bd4c862b1885c9a7b289c18f9024878384c
hamzaqureshie130 <hamzaqureshi11112@gmail.com>
2026-04-26 02:47:08 +0500

    fix(lint): remove unused import and add newlines

--- noshow_iq/api.py
+++ noshow_iq/api.py
@@ -6,7 +6,6 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 from noshow_iq.model import load_model, predict
-from noshow_iq.preprocess import get_features_and_target
 
 load_dotenv()
 
